b02c44cfdd2b31e4bc652a99ad17e7d17071f06e
Hugo TRINH <hugo.trinh-quy@epitech.eu>
2019-04-26 23:09:02 +0200

    create utilities file.

---
+++ utilities.py
@@ -0,0 +1,20 @@
+from sys import stderr
+
+def helper() -> None:
+    """Display the prgram usage."""
+    print('USAGE',
+            '\t./206neutrinos n a h sd\n',
+            'DESCRIPTION',
+            '\tn\tnumber of values',
+            '\ta\tarithmetic mean',
+            '\th\tharmonic mean',
+            '\tsd\tstandard deviation',
+            sep='\n', end='\n')
+
+def error(str: str) -> None:
+    """Write error msg on error output then exits 84."""
+    str = str + '\n'
+    stderr.write(str)
+    exit(84)
+
+
